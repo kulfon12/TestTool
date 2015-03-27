3e7ef9f948dfc7eb0fb78e915f817493f74e3560
Marcin <kulfon12@yahoo.co.uk>
2015-03-27 11:26:20 +0100

    :))))

---
+++ App5/src/App.java
@@ -0,0 +1,24 @@
+
+class Frog {
+	
+	private String name;
+	private int age;
+	
+	public String  getName(){
+		return name;
+		
+		
+	}
+}
+
+
+
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
